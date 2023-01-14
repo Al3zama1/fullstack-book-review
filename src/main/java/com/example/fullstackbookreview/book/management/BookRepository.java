c155b8bf36b86d29457096487fc90608b533c107
Abran Lezama <lezama.abran@gmail.com>
2023-01-13 18:03:07 -0800

    implemented book repository

---
+++ src/main/java/com/example/fullstackbookreview/book/management/BookRepository.java
@@ -0,0 +1,9 @@
+package com.example.fullstackbookreview.book.management;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookRepository extends JpaRepository<Book, Long> {
+    Book findByIsbn(String isbn);
+}
