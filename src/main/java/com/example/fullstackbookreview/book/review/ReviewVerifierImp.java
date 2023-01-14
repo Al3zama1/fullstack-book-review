96b2d51a94289fd42730db4912a36a7bde8623b2
Abran Lezama <lezama.abran@gmail.com>
2023-01-14 11:02:55 -0800

    implemented book review verifier service along with corresponding unit tests

--- src/main/java/com/example/fullstackbookreview/book/review/ReviewVerifierImp.java
+++ src/main/java/com/example/fullstackbookreview/book/review/ReviewVerifierImp.java
@@ -2,10 +2,33 @@ package com.example.fullstackbookreview.book.review;
 
 import org.springframework.stereotype.Service;
 
+import java.util.Arrays;
+
 @Service
 public class ReviewVerifierImp implements ReviewVerifier{
     @Override
     public boolean reviewMeetsQualityStandards(String review) {
-        return false;
+        if (review.contains("Lorem ipsum")) {
+            return false;
+        }
+
+        String[] words = review.split(" ");
+
+        if (Arrays.stream(words).filter(s -> s.equalsIgnoreCase("I")).count() >= 5) {
+            return false;
+        }
+
+        if (Arrays.stream(words).filter(s -> s.equalsIgnoreCase("good")).count() >= 3) {
+            return false;
+        }
+
+        if (words.length <= 10) return false;
+
+        return doesNotContainSwearWords(review);
     }
+
+    private boolean doesNotContainSwearWords(String review) {
+        return !review.contains("shit");
+    }
+
 }
---
+++ src/test/java/com/example/fullstackbookreview/book/review/ReviewVerifierImpTest.java
@@ -0,0 +1,89 @@
+package com.example.fullstackbookreview.book.review;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ReviewVerifierImpTest {
+
+    private ReviewVerifierImp cut;
+
+    @BeforeEach
+    void setup() {
+        cut = new ReviewVerifierImp();
+    }
+
+    @Test
+    void shouldRejectReviewWhenReviewContentContainsLoremIpsum() {
+        // Give
+        String badReview = "Lorem ipsum";
+
+        // When
+        boolean validReview = cut.reviewMeetsQualityStandards(badReview);
+
+        // Then
+        assertThat(validReview).isEqualTo(false);
+    }
+
+    @Test
+    void shouldRejectReviewWhenItContainsMoreThanFiveI() {
+        // Given
+        String badReview = "I like it because I think it is good. I recommend it. I want to read it again because I enjoy it";
+
+        // When
+        boolean validReview = cut.reviewMeetsQualityStandards(badReview);
+
+        // Then
+        assertThat(validReview).isEqualTo(false);
+    }
+
+    @Test
+    void shouldRejectReviewWhenItContainsMoreThanThreeGood() {
+        // Given
+        String badReview = "It is good and I would read it again because it is good and I like it. I recommend it, it is good";
+
+        // When
+        boolean validReview = cut.reviewMeetsQualityStandards(badReview);
+
+        // Then
+        assertThat(validReview).isEqualTo(false);
+    }
+
+    @Test
+    void shouldRejectReviewWhenItContainsLessThanTenCharacters() {
+        // Given
+        String badReview = "Overall good book";
+
+        // When
+        boolean validReview = cut.reviewMeetsQualityStandards(badReview);
+
+        // Then
+        assertThat(validReview).isEqualTo(false);
+    }
+
+    @Test
+    void shouldRejectReviewWhenItContainsSwearWords() {
+        // Given
+        String badReview = "I like this book because it has a lot good examples but I feel like it is also kind of shit";
+
+        // When
+        boolean validReview = cut.reviewMeetsQualityStandards(badReview);
+
+        // Then
+        assertThat(validReview).isEqualTo(false);
+    }
+
+    @Test
+    void shouldAcceptReviewWhenItIsOfGoodQuality() {
+        // Given
+        String goodReview = "Overall I like this book because it has a lot of relatable examples to real life";
+
+        // When
+        boolean validReview = cut.reviewMeetsQualityStandards(goodReview);
+
+        // Then
+        assertThat(validReview).isEqualTo(true);
+    }
+}
